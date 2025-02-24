b5962a43bfd238da3adb0270547519c5709b2817
Terry294-byte <shiroterry168@gmail.com>
2025-02-24 16:32:43 +0300

    added index.js the routes

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express=require('express');
-
+const express = require('express');
+require('dotenv').config();
+require('./helpers/init_mongodb');
 
 const app = express();
 
-const routes = require('./Routes/api');
+const routes = require('./Routes/StudentRoute');
+
+// Add express.json() middleware to parse JSON requests
+app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.port|| 4000,function(){
-    console.log('Now listening for requests on :http://localhost:4000')
-
-
+app.listen(process.env.PORT || 4000, function() {
+    console.log('Now listening for requests on :http://localhost:' + (process.env.PORT || 4000));
 });
-
-
-
