8e0ffc4c7a5de488b7e0c97f8086e2f52d0346f8
Terry294-byte <shiroterry168@gmail.com>
2025-02-25 14:28:31 +0300

    added the getstudentby id,getallstudents,and update student by id in the

--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -1,28 +1,21 @@
 const express =require(
     'express');
+    const StudentController = require('../controllers/StudentController');
 
     const routes = express.Router();
     //get a list of students from the database
-    routes.post('/students', (req, res) => {
-        console.log(req.body); // This should show your student data
-        // Here you would save to MongoDB
-        res.send({
-            type: 'Post Request',
-            receivedData: req.body
-        });
-    });
-
+    routes.get('/getstudents',StudentController.getAllStudents);
 //add a student to the database
 
-routes.post('/students',(req, res)=>{
-    res.send({type:'Post Request'});
-});
+routes.post('/addStudent',StudentController.AddStudent);
 
 //update a student in the database
 
-routes.put('/students/:id',(req, res)=>{
-    res.send({type:'Put Request'});
-});
+routes.put('/updateStudent/:id',StudentController.updateStudent);
+
+//get a student by id
+
+routes.get('/getStudent/:id',StudentController.getStudent);
 
 //delete a student from the database
 
