a1b5e631fe5a6b8c5c52d794327d93066f0707cd
Terry294-byte <shiroterry168@gmail.com>
2025-02-25 14:30:52 +0300

    add the app.use('/api',routes)

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ require('./helpers/init_mongodb');
 
 const app = express();
 
-const routes = require('./Routes/StudentRoute');
+
+
 
 // Add express.json() middleware to parse JSON requests
 app.use(express.json());
-
-app.use(routes);
+const routes = require('./Routes/StudentRoute');
+app.use('/api',routes);
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('Now listening for requests on :http://localhost:' + (process.env.PORT || 4000));
