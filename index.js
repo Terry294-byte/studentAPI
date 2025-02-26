674ed19f39ee0ed3833809002be71b058355a9a1
Terry294-byte <shiroterry168@gmail.com>
2025-02-26 13:01:26 +0300

    included the lecturers routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config();
 require('./helpers/init_mongodb');
 
+const { createError } = require('http-errors');
+
 const app = express();
 
 
@@ -10,7 +12,28 @@ const app = express();
 // Add express.json() middleware to parse JSON requests
 app.use(express.json());
 const routes = require('./Routes/StudentRoute');
+const routes = require('./Routes/LecturersRoutes');
 app.use('/api',routes);
+
+// Error handling 404 error
+app.use(async(req, res, next)=>{
+    //next(createError(404,"Not Found"));
+    next(createError.NotFound());
+}
+);
+//error handler
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+            
+        },
+           
+    });
+});
+
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('Now listening for requests on :http://localhost:' + (process.env.PORT || 4000));
